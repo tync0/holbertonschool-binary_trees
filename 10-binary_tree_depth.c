e87a87a070a2287e1f560404a166d20f9d4792ea
tync0 <isgenderlituncay2004@gmail.com>
2024-01-13 21:44:18 -0800

    Task 1

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,5 +11,5 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (!tree && !tree->parent)
 		return (0);
-	return (++binary_tree_depth(tree->parent));
+	return (1 + binary_tree_depth(tree->parent));
 }
