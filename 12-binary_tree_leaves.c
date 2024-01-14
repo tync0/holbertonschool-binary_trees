57c309a261dd7242b565a3b76b87f0839e591503
tync0 <isgenderlituncay2004@gmail.com>
2024-01-13 23:19:33 -0800

    Task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,7 +12,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (!tree->left && !tree->right && tree->parent)
-		return (1);
+		return (1 + binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right));
 	return (binary_tree_leaves(tree->left) +
 			binary_tree_leaves(tree->right));
 }
