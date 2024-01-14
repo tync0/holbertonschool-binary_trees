9c69d9edbce504cf19bb623ec4f02dd485b077db
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-01-14 17:35:52 +0400

    Create 15-binary_tree_is_full.c

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - function that checks if a binary tree is full
+ * @tree: pointer to the root node
+ * Return: 1 if it full, otherwise 0
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
+	return (0);
+}
