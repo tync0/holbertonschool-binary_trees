5602123fedc19e1aa1988c9b7e53028b32d831fc
tync0 <isgenderlituncay2004@gmail.com>
2024-01-13 20:50:53 -0800

    Task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,9 +10,14 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node;
 
-	if (!node || !parent)
+	if (!parent)
+		return (NULL);
+
+	node = malloc(sizeof(binary_tree_t));
+
+	if (!node)
 		return (NULL);
 	node->n = value;
 	node->parent = parent;
